de55ccec893fe71b7923569e47f597b3459669ab
Rodrigo leyva <leyvarodrigo36@yahoo.com>
2019-02-14 02:49:31 -0800

    added authentications process for users to sign up and sign in

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -7,23 +7,46 @@
 //
 
 import UIKit
-
+import Parse
 class LoginViewController: UIViewController {
     @IBOutlet weak var UsernameField: UITextField!
     @IBOutlet weak var PasswordField: UITextField!
     
-    @IBAction func onSignIn(_ sender: Any) {
-    }
-    
-    @IBAction func onSignUp(_ sender: Any) {
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = UsernameField.text!
+        let password = PasswordField.text!
+        
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error: \(error?.localizedDescription)") 
+            }
+        }
+    }
     
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        let user = PFUser()
+        user.username = UsernameField.text
+        user.password = PasswordField.text
+        
+        user.signUpInBackground { (sucess, error) in
+            if sucess {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
