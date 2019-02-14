e9043380e8f318685750417c49125f78230dded0
Rodrigo leyva <leyvarodrigo36@yahoo.com>
2019-02-14 10:41:23 -0800

    added ability to post to parse data base pictures, comment, and author attributes to schema

--- Parstagram/PostingViewController.swift
+++ Parstagram/PostingViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 class PostingViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var commentField: UITextField!
@@ -25,6 +26,24 @@ class PostingViewController: UIViewController,UIImagePickerControllerDelegate,UI
     
     
     @IBAction func onSubmit(_ sender: Any) {
+        let posts = PFObject(className: "Posts")
+        
+        posts["caption"] = commentField.text!
+        posts["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        posts["image"] = file
+        
+        posts.saveInBackground { (success, error) in
+            if success{
+                self.dismiss(animated: true, completion: nil)
+                print("saved")
+            }else{
+                print("Error")
+            }
+        }
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
