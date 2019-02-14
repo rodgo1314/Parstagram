e5bb6801b3e63bd12e43dfb3e8cc8c109a07706c
Rodrigo leyva <leyvarodrigo36@yahoo.com>
2019-02-14 02:24:30 -0800

    added the assets pngs the buttons no functionality yet

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Rodrigo Leyva on 2/14/19.
+//  Copyright © 2019 Rodrigo Leyva. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var UsernameField: UITextField!
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
